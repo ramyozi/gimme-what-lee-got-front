0422a49503b43c5a7b2daea93c001ddb01f92432
ramzi <ramzi.benmansour@artdigit.fr>
2025-09-14 00:33:09 +0200

    display logout button on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,16 @@
+import {useAuth} from "../lib/plugin/auth-provider.tsx";
+
 export default function Home() {
 
+    const {logout, user} = useAuth()
+
+    const handleLogout = async ()=>{
+        await logout()
+    }
+
   return (
- <div className="container mt-5">
-     home
+    <div className="container mt-5">
+        <button onClick={handleLogout} hidden={!user}> Logout </button>
     </div>
   )
 }
