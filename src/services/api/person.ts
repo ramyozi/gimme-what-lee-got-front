79ccd2a66fcfd957a925c669959c73152e4ffab4
Ramzi <ramzibenmansour.pro@gmail.com>
2025-10-07 14:13:18 +0200

    feat(person): add full CRUD backend and API integration for Person model
    without delete, will be implementd later

--- src/services/api/person.ts
+++ src/services/api/person.ts
@@ -2,15 +2,27 @@ import type { Person } from '../../types';
 import {apiClient} from "../../lib/plugin/auth-provider/api-client.tsx";
 
 // Récupérer toutes les personnes
-export const getPeople = async (limit?: number): Promise<Person[]> => {
-  const res = await apiClient.get<Person[]>('/catalog/person/', {
-    params: limit ? { limit } : {},
-  });
+export const getAllPersons = async (): Promise<Person[]> => {
+  const res = await apiClient.get<Person[]>("/catalog/person/");
   return res.data;
 };
 
 // Récupérer une personne par ID
 export const getPersonById = async (id: string): Promise<Person> => {
   const res = await apiClient.get<Person>(`/catalog/person/${id}/`);
+  return res.data;
+};
+
+export const createPerson = async (data: Omit<Person, "id">): Promise<Person> => {
+  const res = await apiClient.post<Person>("/catalog/person/", data);
+  return res.data;
+};
+
+// Mettre à jour une personne
+export const updatePerson = async (
+    id: string,
+    data: Partial<Person>
+): Promise<Person> => {
+  const res = await apiClient.patch<Person>(`/catalog/person/${id}/`, data);
   return res.data;
 };
